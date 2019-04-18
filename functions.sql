38fc6907a4ca59b205d09c1d1a479770d13b5624
Luis Solís <37953927+solisgb@users.noreply.github.com>
2019-04-18 22:07:04 +0200

    some plpgsql functions

---
+++ functions.sql
@@ -0,0 +1,106 @@
+create or replace function f_is_point(geom1 geometry, verbose1 boolean default false) returns boolean as $$
+/* check if geom1 is point or multipoint 
+   if point or multi returns true
+   else returns false
+*/
+declare 
+	sgtype varchar()
+begin
+	if not st_isvalid(geom1) then
+		if verbose1 then 
+			raise notice 'geom not valid';
+		end if;
+		return false;
+	end if;
+	sgtype := geometrytype(geom1)
+	if geometrytype(geom1) = 'geometrycollection' then
+		if verbose1 then
+			raise notice 'geom is ' || sgtype;
+		end if;
+		return false;
+	end if;
+	if st_dimension(geom1) = 0 then
+		return true;
+	else
+		if verbose1 then
+			raise notice 'geom is ' || geometrytype(geom1);
+		end if;
+		return false;
+	end if;
+end;
+$$ language plpgsql strict immutable;
+
+-----------------------------------------------------------------------------------------------------------
+
+create or replace function ign.f_ca(geom1 geometry, field_name varchar default 'codnut2') returns varchar
+as $function$
+/* returns the value of a varchar field content from table ccaa
+   given a multipoint geometry geom1, geom1 must intersect ccaa.geom of multypoligon type
+   field_name is the name of a varchar field in table ccaa
+*/
+declare
+	textgeom1 varchar;
+	field_value varchar;
+begin
+	textgeom1 = st_astext(geom1);
+	execute format('select %I '
+	'from ccaa '
+	'where st_intersects (st_geomfromtext($1, 25830), ccaa.geom)', field_name)
+	into field_value
+	using textgeom1;
+	return field_value;
+end;
+$function$ language plpgsql strict immutable;
+
+----------------------------------------------------------------------------------------------------------------
+
+create or replace function ign.f_provincia(geom1 geometry, field_name varchar default 'codnut2') returns varchar
+as $function$
+/* returns the value of a varchar field content from table provincias
+   given a multipoint geometry geom1, geom1 must intersect ccaa.geom of multypoligon type
+   field_name is the name of a varchar field in table provincias
+*/
+declare
+	textgeom1 varchar;
+	field_value varchar;
+begin
+	textgeom1 = st_astext(geom1);
+	execute format('select %I '
+	'from provincias '
+	'where st_intersects (st_geomfromtext($1, 25830), provincias.geom)', field_name)
+	into field_value
+	using textgeom1;
+	return field_value;
+end;
+$function$ language plpgsql strict immutable;
+
+--------------------------------------------------------------------------------------------------------------
+
+create or replace function ign.f_provincia_name(geom1 geometry) 
+returns setof character varying
+language 'plpgsql'
+strict immutable
+as $function$ 
+/*devuelve los nombres de las provincias en que se localiza una geometría geom1
+  geom1 debe ser de tipo punto, en caso contrario devuelve null*/
+begin
+	if not st_isvalid(geom1) then
+		return query select 'geom no válida';
+	end if;
+	if geometrytype(geom1) = 'geometrycollection' then
+		return query select 'provincia no disponible para geom collection';
+	end if;
+	if st_dimension(geom1) = 0 then
+		return query
+		select provincias.nameunit
+		from provincias 
+		where st_intersects (geom1, provincias.geom) 
+		union all 
+		select 'fuera de provincias'
+		where not exists (select provincias.nameunit from provincias where st_intersects (geom1, provincias.geom));
+	else
+		return query select 'geom debe ser tipo punto';
+	end if;
+end;
+$function$
+;
