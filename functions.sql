8a76036e1fc44a366cc761fa767509c258a57464
Luis Solís <37953927+solisgb@users.noreply.github.com>
2019-04-20 21:52:31 +0200

    Add files via upload

--- functions.sql
+++ functions.sql
@@ -32,70 +32,85 @@ $$ language plpgsql strict immutable;
 
 -----------------------------------------------------------------------------------------------------------
 
-create or replace function ign.f_cautonoma(geom1 geometry, field_name varchar default 'codnut2'::character varying)
+create or replace function ign.f_cautonoma(geom1 geometry, 
+ field_name varchar default 'codnut1'::varchar)
  returns varchar
 as $function$
 /* returns the value of a varchar field content from table ccaa
    given a multipoint geometry geom1, geom1 must intersect ccaa.geom of multypoligon type
    field_name is the name of a varchar field in table ccaa
+   this function uses special definition of ign tables ccaa, provincias y ttmm
 */
 declare
 textgeom1 varchar;
 field_value varchar;
-field_value2 varchar;
+field_value1 varchar;
 begin
 	textgeom1 = st_astext(geom1);
-	execute format('select %i '
+	--spatial query always on table ttmm (faster) 
+	execute format('select codnut1 '
 	'from ttmm '
-	'where st_intersects (st_geomfromtext($1, 25830), ttmm.geom)', field_name)
+	'where st_intersects (st_geomfromtext($1, 25830), geom)')
 	into field_value
 	using textgeom1;
 
-	execute format('select %i '
+	if field_name = 'codnut1' then
+		return field_value;
+	end if;
+	
+	execute format('select %I '
 	'from ccaa '
-	'where %i = $1', field_name, field_name)
-	into field_value2
+	'where codnut1 = $1', field_name)
+	into field_value1
 	using field_value;
-	return field_value2;
+	return field_value1;
 end;
 $function$ language plpgsql immutable strict;
 ----------------------------------------------------------------------------------------------------------------
 
-create or replace function ign.f_provincia(geom1 geometry, field_name varchar default 'codnut2'::character varying)
+create or replace function ign.f_provincia(geom1 geometry, 
+ field_name varchar default 'codnut2'::varchar)
  returns varchar
 as $function$
 /* returns the value of a varchar field content from table provincias
    given a multipoint geometry geom1, geom1 must intersect provincias.geom of multypoligon type
    field_name is the name of a varchar field in table provincias
+   this function uses special definition of ign tables ccaa, provincias y ttmm
 */
 declare
 textgeom1 varchar;
 field_value varchar;
-field_value2 varchar;
+field_value1 varchar;
 begin
 	textgeom1 = st_astext(geom1);
-	execute format('select %I '
+	--spatial query always on table ttmm (faster)
+	execute format('select codnut2 '
 	'from ttmm '
-	'where st_intersects (st_geomfromtext($1, 25830), ttmm.geom)', field_name)
+	'where st_intersects (st_geomfromtext($1, 25830), geom)')
 	into field_value
 	using textgeom1;
+	
+	if field_name in ('codnut1', 'codnut2') then
+		return field_value;
+	end if;
 
 	execute format('select %I '
 	'from provincias '
-	'where %I = $1', field_name, field_name)
-	into field_value2
+	'where codnut2 = $1', field_name)
+	into field_value1
 	using field_value;
-	return field_value2;
+	return field_value1;
 end;
 $function$ language plpgsql immutable strict;
 
 ----------------------------------------------------------------------------------------------------------------
 
-create or replace function ign.f_tmunicipal(geom1 geometry, field_name varchar default 'codnut2') returns varchar
+create or replace function ign.f_tmunicipal(geom1 geometry, 
+field_name varchar default 'codnut3') returns varchar
 as $function$
-/* returns the value of a varchar field content from table provincias
-   given a multipoint geometry geom1, geom1 must intersect ccaa.geom of multypoligon type
-   field_name is the name of a varchar field in table provincias
+/* returns the value of a varchar field content from table ttmm
+   given a multipoint geometry geom1, geom1 must intersect ttmm.geom of multypoligon type
+   field_name is the name of a varchar field in table ttmm
 */
 declare
 	textgeom1 varchar;
@@ -104,7 +119,30 @@ begin
 	textgeom1 = st_astext(geom1);
 	execute format('select %I '
 	'from ttmm '
-	'where st_intersects (st_geomfromtext($1, 25830), ttmm.geom)', field_name)
+	'where st_intersects (st_geomfromtext($1, 25830), geom)', field_name)
+	into field_value
+	using textgeom1;
+	return field_value;
+end;
+$function$ language plpgsql strict immutable;
+
+----------------------------------------------------------------------------------------------------------------
+
+create or replace function ign.f_mtn25(geom1 geometry, 
+field_name varchar default 'mtn25_clas') returns varchar
+as $function$
+/* returns the value of a varchar field content from table cmtn25
+   given a multipoint geometry geom1, geom1 must intersect ttmm.geom of multypoligon type
+   field_name is the name of a varchar field in table ttmm
+*/
+declare
+	textgeom1 varchar;
+	field_value varchar;
+begin
+	textgeom1 = st_astext(geom1);
+	execute format('select %I '
+	'from cmtn25 '
+	'where st_intersects (st_geomfromtext($1, 25830), geom)', field_name)
 	into field_value
 	using textgeom1;
 	return field_value;
@@ -113,31 +151,4 @@ $function$ language plpgsql strict immutable;
 
 --------------------------------------------------------------------------------------------------------------
 
-create or replace function ign.f_provincia_name(geom1 geometry) 
-returns setof character varying
-language 'plpgsql'
-strict immutable
-as $function$ 
-/*devuelve los nombres de las provincias en que se localiza una geometría geom1
-  geom1 debe ser de tipo punto, en caso contrario devuelve null*/
-begin
-	if not st_isvalid(geom1) then
-		return query select 'geom no válida';
-	end if;
-	if geometrytype(geom1) = 'geometrycollection' then
-		return query select 'provincia no disponible para geom collection';
-	end if;
-	if st_dimension(geom1) = 0 then
-		return query
-		select provincias.nameunit
-		from provincias 
-		where st_intersects (geom1, provincias.geom) 
-		union all 
-		select 'fuera de provincias'
-		where not exists (select provincias.nameunit from provincias where st_intersects (geom1, provincias.geom));
-	else
-		return query select 'geom debe ser tipo punto';
-	end if;
-end;
-$function$
-;
+
